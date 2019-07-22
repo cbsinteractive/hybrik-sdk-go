8ac2bae34e0bf60135892e60fb7cd9e618e8b7b7
Thomas Symborski <thomas.symborski@gmail.com>
2019-07-22 15:32:44 -0400

    models: adds default versions for DoVi tools

--- job_models.go
+++ job_models.go
@@ -1,5 +1,13 @@
 package hybrik
 
+const (
+	versionDoViVesQCVersionDefault  = "0.9.0.9"
+	versionDoViMezzQCVersionDefault = "2.6.2"
+	versionDoViMP4QCDefault         = "1.1.4"
+	versionDoViMP4MuxerDefault      = "1.2.8"
+	versionDoViSDKVersionDefault    = "4.2.1_qa"
+)
+
 // CreateJob .
 type CreateJob struct {
 	Name              string           `json:"name"`
